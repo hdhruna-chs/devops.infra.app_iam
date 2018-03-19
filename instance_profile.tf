97e0ceafc8ba437d027e7d467be0fe80c9449a06
Sam Flint <sam.flint@corvesta.com>
2018-03-19 17:02:04 -0400

    updat app_iam

--- instance_profile.tf
+++ instance_profile.tf
@@ -1,4 +1,14 @@
+resource "aws_iam_instance_profile" "logstash_instance" {
+  name = "logstash-instance"
+  role = "${module.ec2_logstash_role.role_name}"
+}
+
 resource "aws_iam_instance_profile" "rabbitmq" {
   name = "rabbitmq"
   role = "${module.rabbitmq_role.role_name}"
+}
+
+resource "aws_iam_instance_profile" "nexpose_scanner" {
+  name = "nexpose_scanner"
+  role = "${module.nexpose_role.role_name}"
 }
