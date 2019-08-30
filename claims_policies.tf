131edefb563d9777186add50b836817b5039b942
Sam Flint <sam.flint@corvesta.com>
2019-08-30 11:49:06 -0400

    update typo

--- claims_policies.tf
+++ claims_policies.tf
@@ -27,15 +27,15 @@ vars = {
 #Claims APP policy and attachemnt
 resource "aws_iam_role_policy_attachment" "claims" {
   role       = module.ecs_claims_api_role.role_name
-  policy_arn = aws_iam_policy.claims_trigger_lambda_policy.arn
+  policy_arn = aws_iam_policy.claims_policy.arn
 }
 
-resource "aws_iam_policy" "claims_trigger_lambda_policy" {
+resource "aws_iam_policy" "claims_policy" {
   name   = "${data.terraform_remote_state.config.outputs.run_env}.claims-policy"
-  policy = data.template_file.claims_trigger_lambda_policy.rendered
+  policy = data.template_file.claims_policy.rendered
 }
 
-data "template_file" "claims_trigger_lambda_policy" {
+data "template_file" "claims_policy" {
 template = file("${path.module}/policies/s3_trigger_lambda.json.tpl")
 
 vars = {
--- ice_polices.tf
+++ ice_polices.tf
@@ -24,19 +24,19 @@ vars = {
   }
 }
 
-#Ice APP policy and attachemnt
-resource "aws_iam_role_policy_attachment" "ice_trigger_lambda_vpc_access" {
-  role       = module.ice_bucket_role.role_name
-  policy_arn = data.aws_iam_policy.lambda_vpc_access_policy.arn
-}
-
-
-data "template_file" "ice_trigger_lambda_policy" {
-template = file("${path.module}/policies/read_write_s3_bucket.json.tpl")
-
-vars = {
-  env         = data.terraform_remote_state.config.outputs.run_env
-  region      = data.terraform_remote_state.config.outputs.default_region
-  bucket_name = data.terraform_remote_state.buckets.outputs.ice_bucket_id
-  }
-}
+# #Ice APP policy and attachemnt
+# resource "aws_iam_role_policy_attachment" "ice_trigger_lambda_vpc_access" {
+#   role       = module.ice_bucket_role.role_name
+#   policy_arn = data.aws_iam_policy.lambda_vpc_access_policy.arn
+# }
+#
+#
+# data "template_file" "ice_trigger_lambda_policy" {
+# template = file("${path.module}/policies/read_write_s3_bucket.json.tpl")
+#
+# vars = {
+#   env         = data.terraform_remote_state.config.outputs.run_env
+#   region      = data.terraform_remote_state.config.outputs.default_region
+#   bucket_name = data.terraform_remote_state.buckets.outputs.ice_bucket_id
+#   }
+# }
