50e520aef6e50a0f88072a81e9a33881bfdaa8ba
Justin Arnold <jarnold@va.cinc.local>
2019-10-01 10:18:32 -0400

    removing variables from name

--- rds_enhanced_monitoring.tf
+++ rds_enhanced_monitoring.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "rds_enhanced_monitoring" {
-  name  = "${var.run_env}-${var.name}-rds-enhanced-monitoring-role"
+  name  = "rds-enhanced-monitoring-role"
 
   # Trust policy...
   assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
