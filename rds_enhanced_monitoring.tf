2beca443fdaf396052810a1ec42607ec50399df7
Justin Arnold <jarnold@va.cinc.local>
2019-10-01 10:04:10 -0400

    moving here so a single role can be created

---
+++ rds_enhanced_monitoring.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "rds_enhanced_monitoring" {
+  name  = "${var.run_env}-${var.name}-rds-enhanced-monitoring-role"
+
+  # Trust policy...
+  assume_role_policy = data.aws_iam_policy_document.rds_enhanced_monitoring.json
+}
+
+resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
+  role  = aws_iam_role.rds_enhanced_monitoring.name
+
+  # Permissions policy...
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
+data "aws_iam_policy_document" "rds_enhanced_monitoring" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["monitoring.rds.amazonaws.com"]
+    }
+  }
+}
